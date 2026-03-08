ff283d2a742bb64b3e63487463cdb9644b931eac
Pilot010 <cik19281312812812708472@gmail.com>
2026-03-08 16:49:01 -0700

    added more stuff

--- The Cliker/Data/GeneralGameData.swift
+++ The Cliker/Data/GeneralGameData.swift
@@ -12,14 +12,45 @@ import Combine
 class GeneralGameData: ObservableObject {
    
     @Published var totalClicks: Int = 0
-    @Published var totalUpgrades: AllUpgradesBlueprint = AllUpgradesBlueprint(numOfExtraClickers: 0)
- 
+    @Published var totalUpgrades: AllUpgradesBlueprint = AllUpgradesBlueprint(allBuildings: [.numOfClickers: 0])
     
+    
+    //MARK: all the increases 
+    @Published var clickerIncrease: Int = 1
+        
+    
+    ///Per Second
+    func updateTotalClicks() {
+        
+        var amount: Int = 0
+
+        for (building, Amount) in totalUpgrades.allBuildings {
+            
+            switch building {
+                
+            case .numOfClickers:
+                amount += clickerIncrease * Amount
+ 
+            }
+        }
+
+        totalClicks += amount
+    }
+    
+    enum AllBuildingsBlueprint_blueprint {
+        
+        case numOfClickers
+        
+        
+    }
+    
+    //for expansion
     struct AllUpgradesBlueprint {
         
-        var numOfExtraClickers: Int = 0
-        init(numOfExtraClickers: Int) {
-            self.numOfExtraClickers = numOfExtraClickers
+        var allBuildings: [AllBuildingsBlueprint_blueprint:Int] = [:]
+        
+        init(allBuildings: [AllBuildingsBlueprint_blueprint:Int]) {
+            self.allBuildings = allBuildings
         }
         
     }
