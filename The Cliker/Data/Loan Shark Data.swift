2ac78d7a7523bf5c950e887f44624118f0418df4
Pilot010 <cik19281312812812708472@gmail.com>
2026-03-11 07:41:55 -0700

    adde loansharks

---
+++ The Cliker/Data/Loan Shark Data.swift
@@ -0,0 +1,17 @@
+//
+//  Loan Shark Data.swift
+//  The Cliker
+//
+//  Created by Desire on 2026-03-11.
+//
+
+
+import Foundation
+import Combine
+
+class LoanSharkData: ObservableObject {
+    
+    @Published var numOfLoanSharks: Int = 0
+    @Published var typeOfLoanSharks: [AllBuildingsBlueprint] = []
+    
+}
